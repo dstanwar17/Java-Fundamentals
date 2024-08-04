41a3aba3ee8c968a674e51be30c77bd2420aaab7
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-04 14:11:20 +0530

    Exercise 2 - Conditions_loop

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.conditions_loops.labs;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -16,6 +19,34 @@ public class Exercise_02 {
     public static void main(String[] args) {
     
         // write completed here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number between 1 to 7 to find the day of the week: ");
+        int day = sc.nextInt();
+
+        if (day <= 7) {
+            if (day >= 1) {
+                if (day == 1) {
+                    System.out.println("It is Monday");
+                } else if (day == 2) {
+                    System.out.println("It is Tuesday");
+                } else if (day == 3) {
+                    System.out.println("It is Wednesday");
+                } else if (day == 4) {
+                    System.out.println("It is Thursday");
+                } else if (day == 5) {
+                    System.out.println("It is Friday");
+                } else if (day == 6) {
+                    System.out.println("It is Saturday");
+                } else {
+                    System.out.println("It is Sunday");
+                }
+            } else {
+                System.out.println("Please enter a number between 1 to 7 to find the day of the week: ");
+            }
+
+        } else {
+            System.out.println("Please enter a number between 1 to 7 to find the day of the week: ");
+        }
 
     }
 }
