a677c5b22016ce18f2668e47c3fa47b045678527
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-04 20:48:32 +0530

    Exercise 7 - Conditions_loop

--- src/Practice/WhileEx.java
+++ src/Practice/WhileEx.java
@@ -1,21 +1,11 @@
-package labs_examples.conditions_loops.labs;
+package Practice;
 
 import java.util.Scanner;
 
 /**
- * Conditions and Loops Exercise 7: First vowel
- *
- *      Take in a word from the user and using a "while" loop, find the first vowel in the word.
- *      Once you find the vowel, print out the word and the first vowel.
- *
- *      Hints:
- *          - there are a few helpful methods in the String class called length(), charAt() and indexOf()
- *          - you'll likely want to use a String that contains all the vowels:
- *              - ie: String vowels = "aeiou";
- *
+ * @author dstanwar17
  */
-
-public class Exercise_07 {
+public class WhileEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any word and I will tell you the first vowel of it: : ");
@@ -46,5 +36,4 @@ public class Exercise_07 {
         System.out.println(blank.charAt(0));
 
     }
-
 }
