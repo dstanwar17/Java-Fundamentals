bce2f3da738236705d4f2f502689f798c9f75230
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-04 21:18:01 +0530

    Exercise 9 to 11 - conditions_loop

--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                break;
+            } else {
+                System.out.println(i + " is not even");
+            }
+        }
+    }
 }
