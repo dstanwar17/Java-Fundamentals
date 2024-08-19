805e460a341dbeb3848cdee08df074b8a04b56f5
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-20 02:31:33 +0530

    Practice Exercise

--- src/Practice/Arrays/NestedArray.java
+++ src/Practice/Arrays/NestedArray.java
@@ -1,4 +1,24 @@
-package Practice.Arrays;/**
+package Practice.Arrays;
+
+/**
  * @author dstanwar17
- */public class NestedArray {
+ */
+public class NestedArray {
+    public static void main(String[] args) {
+        int[][] firstArray = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < firstArray[i].length; j++) {
+                System.out.print((firstArray[i][j] = i * j) + " ");
+
+            }
+            System.out.println();
+        }
+//        for (int i = 0; i < someArray.length; i++) {
+//            for (int j = 0; j < someArray[i].length; j++) {
+//                System.out.print(someArray[i][j] + " ");
+//            }
+//            System.out.println(); // Move to the next line after each row
+//        }
+    }
 }
