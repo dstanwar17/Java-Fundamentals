658da79a68b18886831493f000c39e4db2d9ecd0
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-26 19:53:49 +0530

    Practice Commit

---
+++ src/Practice/classes/AccessMod.java
@@ -0,0 +1,32 @@
+package Practice.classes;
+
+/**
+ * @author dstanwar17
+ */
+public class AccessMod {
+
+    public int publicVar;
+    protected int protectedVar;
+    private int privateVar;
+
+    /*
+        Access Modifiers - affect classes, instance variables and methods. They play a large role in Encapsulation;
+                          one of the primary tenants of Object-Oriented Programming.
+
+        public -    any class, instance variable or method that is marked as "public" can be openly accessible from
+                    anywhere in the application. It can also be accessed by external applications that are using this
+                    application.
+
+        private -   only accessible from within the class where the private variable or method is declared. We do not
+                    have a thing such as private class.
+
+        protected - only accessible by classes that are declared in the same package as the variable or method that is
+                    declared "protected". In addition, subclasses whether they are in the same packages or not, can
+                    access protected members.
+
+        default -   same as protected but not accessible by subclasses.
+
+
+     */
+
+}
---
+++ src/Practice/classes/ClassOne.java
@@ -0,0 +1,15 @@
+package Practice.classes;
+
+/**
+ * @author dstanwar17
+ */
+public class ClassOne {
+    public static void main(String[] args) {
+        AccessMod classOne = new AccessMod();
+
+        classOne.protectedVar = 10;
+        classOne.publicVar = 20;
+
+
+    }
+}
