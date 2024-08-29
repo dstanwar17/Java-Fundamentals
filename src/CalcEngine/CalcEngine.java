438f9434009922b05c3469e2fdd5d7441fb35d50
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-29 23:09:40 +0530

    CalcEngine - If/Else Statement

--- src/CalcEngine/CalcEngine.java
+++ src/CalcEngine/CalcEngine.java
@@ -1,14 +1,28 @@
-package Practice;
+package CalcEngine;
+
+import java.util.Scanner;
 
 /**
  * @author dstanwar17
  */
 public class CalcEngine {
     public static void main(String[] args) {
+
+        // Operating Calculations using If-Else Statements.
+
         double val1;
         double val2;
         double result;
         char opCode;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        val1 = sc.nextDouble();
+        System.out.println("Enter the second number: ");
+        val2 = sc.nextDouble();
+        System.out.println("Enter the operator: ");
+        opCode = sc.next().charAt(0);
+
 
         if (opCode == 'a')
             result = val1 + val2;
