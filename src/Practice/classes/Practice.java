037eff0eca0b6f6454d58b34174c762295b185ee
dstanwar17 <devendra.tanwar@icloud.com>
2024-08-31 19:01:24 +0530

    Practice Commit

--- src/Practice/classes/Practice.java
+++ src/Practice/classes/Practice.java
@@ -1,10 +1,20 @@
 package Practice.classes;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 /**
  * @author dstanwar17
  */
 public class Practice {
     public static void main(String[] args) {
-
+        getCurrentTime();
     }
+
+    public static void getCurrentTime() {
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        System.out.println("Today, it is " + date + " & the current time is : " + time);
+    }
+
 }
