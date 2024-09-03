c8e9a13367f1095d13b7896a85d411c228813a70
dstanwar17 <devendra.tanwar@icloud.com>
2024-09-03 16:58:05 +0530

    Practice Commit

--- src/Practice/loops/DeleteMeLater.java
+++ src/Practice/loops/DeleteMeLater.java
@@ -1,12 +1,17 @@
 package Practice.loops;
 
+import static Practice.loops.Print.print;
+
 /**
  * @author dstanwar17
  */
 public class DeleteMeLater {
     public static void main(String[] args) {
 
-        Print print = new Print();
-        print.print("Hello World!", "How are you doing? ", "My DOB is :", 17, "July", 1989, new int[] {1,2,3,4,5,6,7,8,9,10} );
+        print("Hello World!", "How are you doing? ", "My DOB is :", 17, "July", 1989, new int[] {1,2,3,4,5,6,7,8,9,10} );
+
+        print(1,2,3,4,5,6,7,8,9,0);
+        print("Hello World!");
+        print(new double[]{1,2,3,4,5,6,4,6});
     }
 }
